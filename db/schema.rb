8b2144bbaf7083bb419fe795e7879641ef384036
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-08-18 16:51:30 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180818163449) do
+ActiveRecord::Schema.define(version: 20180818164952) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -25,5 +25,16 @@ ActiveRecord::Schema.define(version: 20180818163449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "artist_id_id"
+    t.integer  "genre_id_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "songs", ["artist_id_id"], name: "index_songs_on_artist_id_id"
+  add_index "songs", ["genre_id_id"], name: "index_songs_on_genre_id_id"
 
 end
